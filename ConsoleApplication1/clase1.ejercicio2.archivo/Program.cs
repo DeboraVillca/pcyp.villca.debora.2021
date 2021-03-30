4674b0bef80736637a34b12e5c49844d27116442
Debora Villca <debyvillca95@gmail.com>
2021-03-30 20:38:55 -0300

    test file reading

--- ConsoleApplication1/clase1.ejercicio2.archivo/Program.cs
+++ ConsoleApplication1/clase1.ejercicio2.archivo/Program.cs
@@ -11,8 +11,34 @@ namespace clase1.ejercicio2.archivo
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello world");
-            Console.ReadLine();
+            String line;
+            try
+            {
+                
+                StreamReader sw = new StreamReader("C:\\Users\\Daniel\\Documents\\GitHub\\pcyp.villca.debora.2021\\ConsoleApplication1\\clase1.ejercicio2.archivo\\vial.csv");
+               
+                //Read the first line of text
+                line = sw.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sw.ReadLine();
+                }
+                //close the file
+                sw.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
