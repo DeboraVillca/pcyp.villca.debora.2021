913792da8affa4b219d23fea6ee217986387ff4f
Debora Villca <debyvillca95@gmail.com>
2021-04-03 16:43:44 -0300

    implement method print

--- ConsoleApplication1/clase1.ejercicio2.archivo/Archivo.cs
+++ ConsoleApplication1/clase1.ejercicio2.archivo/Archivo.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace clase1.ejercicio2.archivo
 {
     class Archivo 
     {
-        int númerodehoja;
-        string tipoobjeto;
-        int identificadortramo;
-        string tipocamino=null;
-        int longitud = 0;
+        int número_dehoja;
+        string tipo_objeto;
+        int identificador_tramo;
+        string tipo_camino;
+        int longitud;
 
+         public void imprimir(){ 
+        String line;
+            try
+            {
+                StreamReader sw = new StreamReader("C:\\Users\\Daniel\\Documents\\GitHub\\pcyp.villca.debora.2021\\ConsoleApplication1\\clase1.ejercicio2.archivo\\vial.csv");
+        line = sw.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sw.ReadLine();
+                }
+    sw.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
 
+           }
+        }
     }
 }
--- ConsoleApplication1/clase1.ejercicio2.archivo/Main.cs
+++ ConsoleApplication1/clase1.ejercicio2.archivo/Main.cs
@@ -11,27 +11,10 @@ namespace clase1.ejercicio2.archivo
     {
         static void Main(string[] args)
         {
-            String line;
-            try
-            {
-                StreamReader sw = new StreamReader("C:\\Users\\Daniel\\Documents\\GitHub\\pcyp.villca.debora.2021\\ConsoleApplication1\\clase1.ejercicio2.archivo\\vial.csv");
-                line = sw.ReadLine();
-                while (line != null)
-                {
-                    Console.WriteLine(line);
-                    line = sw.ReadLine();
-                }
-                sw.Close();
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
+             Archivo arch = new Archivo();
+            arch.imprimir();
+
+
         }
     }
 }
