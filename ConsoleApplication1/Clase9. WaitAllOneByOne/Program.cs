382523fa90d0fb60beb3c20a40dd68204d3a6921
DeboraVillca <debyvillca95@gmail.com>
2021-06-23 23:40:00 -0300

    delete

---
+++ ConsoleApplication1/Clase9. WaitAllOneByOne/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clase9.WaitAllOneByOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+
+            sw.Start();
+
+            int suma = 0;
+            Task t1 = Task.Factory.StartNew((arg) => {
+                Thread.Sleep(1000);
+                int sumaT = (int)arg + 10;
+                return sumaT;
+            }, suma);
+            Task t2 = Task.Factory.StartNew((arg) => {
+                Thread.Sleep(1000);
+                Thread.Sleep(1000);
+                int sumaT = (int)arg + 10;
+                return sumaT;
+            }, suma);
+
+            int cantidadTareas = 2;
+            Task[] tasks = new Task[] { t1, t2 };
+            int indice;
+            while (cantidadTareas > 0)
+            {
+                indice = Task.WaitAny(tasks);
+                cantidadTareas--;
+                suma += tasks[indice].Result;
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("El total es {0} en {1} segundos", suma, (sw.ElapsedMilliseconds / 1000));
+        }
+    }
+}
--- ConsoleApplication1/clase5.multiplicacion.matrices/Program.cs
+++ ConsoleApplication1/clase5.multiplicacion.matrices/Program.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace clase5.multiplicacion.matrices
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
-}
+﻿
